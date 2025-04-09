0ed3866dc071599aba2010d22cb2b03debd32a03
Ivo Jérôme Hartwig <i.hartwig@hbk-bs.de>
2025-04-09 16:45:16 +0200

    mugshot page

---
+++ mugshot/index.js
@@ -0,0 +1,71 @@
+// Classifier Variable
+let classifier;
+// Model URL
+// HERE
+let imageModelURL = 'https://teachablemachine.withgoogle.com/models/p4Hls1KC-/';
+
+// Video
+let video;
+let flippedVideo;
+// To store the classification
+let label = '';
+
+// Load the model first
+function preload() {
+	classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+	console.log(classifier);
+}
+
+function setup() {
+	createCanvas(320, 240);
+
+	// Create the video
+	video = createCapture(VIDEO);
+	video.size(320, 240);
+	video.hide();
+
+	// Start classifying
+	classifyVideo();
+}
+
+function draw() {
+	background(0);
+	// Draw the video
+	image(video, 0, 0);
+
+}
+
+// Get a prediction for the current video frame
+function classifyVideo() {
+	classifier.classify(video, gotResult);
+}
+
+// When we get a result
+function gotResult(results) {
+	console.log(results); // Hier siehst du, wie genau deine Labels heißen
+
+	// Reset Werte
+	let pauline = 0;
+	let andrea = 0;
+	let luca = 0;
+
+	// Gehe alle Vorhersagen durch
+	results.forEach(result => {
+		let label = result.label.toUpperCase(); // Vorsichtshalber alles groß
+		let confidence = result.confidence;
+
+		if (label === "PAULINE") pauline = confidence;
+		if (label === "ANDREA") andrea = confidence;
+		if (label === "LUCA") luca = confidence;
+	});
+
+	// Update die Prozentzahlen im HTML
+	document.getElementById("pauline-val").innerText = Math.round(pauline * 100) + "%";
+	document.getElementById("andrea-val").innerText = Math.round(andrea * 100) + "%";
+	document.getElementById("luca-val").innerText = Math.round(luca * 100) + "%";
+
+	// Und weiter geht's
+	classifyVideo();
+}
+
+  
